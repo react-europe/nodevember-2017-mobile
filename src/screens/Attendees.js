5625d0d6f00bfe7bfc558efde4d56de94c6a1280
MarkLyck <mark.lyck@gmail.com>
2018-05-19 11:09:07 +0200

    implement graphql query using react-apollo

--- src/screens/Attendees.js
+++ src/screens/Attendees.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { Query } from 'react-apollo';
+import GET_ATTENDEES from '../data/attendeesquery'
 
 class Attendees extends Component {
     render() {
-        return <div styled={{ height: '100%', background: 'red', width: '100%'}}>ATTENDEES</div>
+        return <Query query={GET_ATTENDEES}>
+            {({ loading, error, data }) => {
+                if (loading) return <Text>Loading...</Text>
+                if (error) return <Text>Error ${error}</Text>
+
+                const attendees = data && data.events && data.events[0] ? data.events[0].attendees : []
+                console.log('attendees', attendees)
+
+                return <Text>ATTENDEES</Text>
+            }}
+        </Query>
+        
+        
     }
 }
 
-export default Attendees
+export default Attendees;
