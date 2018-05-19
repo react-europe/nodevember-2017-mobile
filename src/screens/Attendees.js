f755c3a3fb312977d0cb1f91c9a87dae525cdd8e
MarkLyck <mark.lyck@gmail.com>
2018-05-19 14:09:59 +0200

    queryThrottle

--- src/screens/Attendees.js
+++ src/screens/Attendees.js
@@ -126,6 +126,13 @@ class DeferredAttendeesContent extends React.Component {
       .replace('twitter.com/', '');
   };
 
+  throttleDelayMs = 50
+  throttleTimeout = null
+  queryThrottle = text => {
+    clearTimeout(this.throttleTimeout);
+    this.throttleTimeout = setTimeout(() => this.setState({ query: text }), this.throttleDelayMs);
+  }
+
   _renderItem = ({ item: attendee }) => (
     <ContactCard key={attendee.id} contact={attendee} />
   );
