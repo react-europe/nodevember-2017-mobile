34e03721db0c43ef710e7909424dd57da2f282fb
Horacio Herrera <hherrerag@me.com>
2018-05-25 20:24:11 +0200

    prettier

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,61 +1,61 @@
-import React from "react";
-import PropTypes from "prop-types";
-import _ from "lodash";
-import { View, Text, StatusBar } from "react-native";
-import { TabNavigator, StackNavigator } from "react-navigation";
+import React from 'react'
+import PropTypes from 'prop-types'
+import _ from 'lodash'
+import { View, Text, StatusBar } from 'react-native'
+import { TabNavigator, StackNavigator } from 'react-navigation'
 
-import Schedule from "./data/schedule.json";
-import moment from "moment";
-import { Colors, FontSizes, Layout } from "./constants";
-import TabBarBottom from "./components/TabBarBottom";
+import Schedule from './data/schedule.json'
+import moment from 'moment'
+import { Colors, FontSizes, Layout } from './constants'
+import TabBarBottom from './components/TabBarBottom'
 
-import Screens from "./screens";
-import QRScannerModalNavigation from "./screens/QRScreens/Identify";
-import QRCheckinScannerModalNavigation from "./screens/QRScreens/CheckIn";
-import QRContactScannerModalNavigation from "./screens/QRScreens/Contact";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import Screens from './screens'
+import QRScannerModalNavigation from './screens/QRScreens/Identify'
+import QRCheckinScannerModalNavigation from './screens/QRScreens/CheckIn'
+import QRContactScannerModalNavigation from './screens/QRScreens/Contact'
+import Ionicons from '@expo/vector-icons/Ionicons'
 
-const FullSchedule = Schedule.events[0].groupedSchedule;
-let navSchedule = {};
+const FullSchedule = Schedule.events[0].groupedSchedule
+let navSchedule = {}
 _.each(FullSchedule, (day, i) => {
   navSchedule[day.title] = {
     screen: Screens.ScheduleDay({
       day: day.title,
-      date: moment(new Date(day.date)).format("ddd")
+      date: moment(new Date(day.date)).format('ddd')
     })
-  };
-});
+  }
+})
 
 const ScheduleNavigation = TabNavigator(navSchedule, {
   lazy: false,
   swipeEnabled: true,
   animationEnabled: true,
   tabBarComponent: TabBarBottom,
-  tabBarPosition: "top",
+  tabBarPosition: 'top',
   tabBarOptions: {
-    style: { backgroundColor: "#333" },
-    activeTintColor: "#fff",
+    style: { backgroundColor: '#333' },
+    activeTintColor: '#fff',
     showLabel: false
   },
   navigationOptions: ({ navigation }) => ({
     tabBarIcon: ({ focused, tintColor }) => {
-      const { routeName } = navigation.state;
+      const { routeName } = navigation.state
       // You can return any component that you like here! We usually use an
       // icon component from react-native-vector-icons
       return (
-        <Text style={{ color: tintColor, fontWeight: "800", fontSize: 18 }}>
+        <Text style={{ color: tintColor, fontWeight: '800', fontSize: 18 }}>
           {routeName.substring(0, 3).toUpperCase()}
         </Text>
-      );
+      )
     }
   })
-});
+})
 
 const DefaultStackConfig = {
   cardStyle: {
-    backgroundColor: "#fafafa"
+    backgroundColor: '#fafafa'
   }
-};
+}
 
 const MenuNavigation = StackNavigator(
   {
@@ -70,52 +70,52 @@ const MenuNavigation = StackNavigator(
     ...DefaultStackConfig,
     navigationOptions: {
       headerStyle: { backgroundColor: Colors.blue },
-      headerTintColor: "white"
+      headerTintColor: 'white'
     }
   }
-);
+)
 
 const ScheduleStackNavigator = StackNavigator({
   Schedule: {
     screen: ScheduleNavigation,
     navigationOptions: {
-      title: "Schedule",
+      title: 'Schedule',
       headerStyle: { backgroundColor: Colors.blue },
-      headerTintColor: "white",
+      headerTintColor: 'white',
       headerTitleStyle: {
-        fontFamily: "open-sans-bold"
+        fontFamily: 'open-sans-bold'
       }
     }
   }
-});
+})
 
 const ProfileNavigator = StackNavigator({
   Profile: {
     screen: Screens.Profile,
     navigationOptions: {
-      title: "Profile",
+      title: 'Profile',
       headerStyle: { backgroundColor: Colors.blue },
-      headerTintColor: "white",
+      headerTintColor: 'white',
       headerTitleStyle: {
-        fontFamily: "open-sans-bold"
+        fontFamily: 'open-sans-bold'
       }
     }
   }
-});
+})
 
 const ContactsNavigator = StackNavigator({
   Contacts: {
     screen: Screens.Contacts,
     navigationOptions: {
-      title: "Contacts",
+      title: 'Contacts',
       headerStyle: { backgroundColor: Colors.blue },
-      headerTintColor: "white",
+      headerTintColor: 'white',
       headerTitleStyle: {
-        fontFamily: "open-sans-bold"
+        fontFamily: 'open-sans-bold'
       }
     }
   }
-});
+})
 
 const PrimaryTabNavigator = TabNavigator(
   {
@@ -126,9 +126,9 @@ const PrimaryTabNavigator = TabNavigator(
     Schedule: {
       screen: ScheduleStackNavigator,
       navigationOptions: {
-        title: "Schedule",
+        title: 'Schedule',
         headerTitleStyle: {
-          fontFamily: "open-sans-bold"
+          fontFamily: 'open-sans-bold'
         }
       }
     },
@@ -138,31 +138,31 @@ const PrimaryTabNavigator = TabNavigator(
   {
     navigationOptions: ({ navigation }) => ({
       tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === "Home") {
-          iconName = "ios-home";
-        } else if (routeName === "Profile") {
-          iconName = `ios-contact${focused ? "" : "-outline"}`;
-        } else if (routeName === "Schedule") {
-          iconName = `ios-calendar${focused ? "" : "-outline"}`;
-        } else if (routeName === "Contacts") {
-          iconName = `ios-contacts${focused ? "" : "-outline"}`;
-        } else if (routeName === "Menu") {
-          iconName = "md-menu";
+        const { routeName } = navigation.state
+        let iconName
+        if (routeName === 'Home') {
+          iconName = 'ios-home'
+        } else if (routeName === 'Profile') {
+          iconName = `ios-contact${focused ? '' : '-outline'}`
+        } else if (routeName === 'Schedule') {
+          iconName = `ios-calendar${focused ? '' : '-outline'}`
+        } else if (routeName === 'Contacts') {
+          iconName = `ios-contacts${focused ? '' : '-outline'}`
+        } else if (routeName === 'Menu') {
+          iconName = 'md-menu'
         }
 
         // You can return any component that you like here! We usually use an
         // icon component from react-native-vector-icons
-        return <Ionicons name={iconName} size={32} color={tintColor} />;
+        return <Ionicons name={iconName} size={32} color={tintColor} />
       }
     }),
     tabBarOptions: {
-      style: { backgroundColor: "#333" },
-      activeTintColor: "#fff"
+      style: { backgroundColor: '#333' },
+      activeTintColor: '#fff'
     }
   }
-);
+)
 
 export default StackNavigator(
   {
@@ -178,7 +178,7 @@ export default StackNavigator(
   },
   {
     ...DefaultStackConfig,
-    headerMode: "none",
-    mode: "modal"
+    headerMode: 'none',
+    mode: 'modal'
   }
-);
+)
