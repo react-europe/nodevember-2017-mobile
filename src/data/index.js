c40292651c4fbab7fe012335799a6b28865a3994
Brent Vatne <brentvatne@gmail.com>
2019-05-20 09:55:10 +0200

    Fix next talk filter function

--- src/data/index.js
+++ src/data/index.js
@@ -1,17 +1,22 @@
 import _ from 'lodash';
 import moment from 'moment-timezone';
+import {convertUtcDateToEventTimezone} from '../utils';
 
-export function findNextTalksAfterDate(event, date = new Date()) {
+export function findNextTalksAfterDate(event) {
   let flattenedTalks = event.groupedSchedule.reduce((result, group) => {
     result = [...result, ...group.slots];
     return result;
   }, []);
 
+  let currentTime = moment.tz(event.timezoneId);
+  // You can test going into the future with:
+  // let currentTime = moment.tz(event.timezoneId).add(4, 'days');
+
   let talks = _.filter(
     flattenedTalks,
     talk =>
-      (talk.type === 0 || talk.type === 1) &&
-      true
+      (talk.type === 0 || talk.type === 1 || talk.type === 8) &&
+      currentTime.isBefore(convertUtcDateToEventTimezone(talk.startDate))
   );
   return talks.slice(0, 3);
 }
