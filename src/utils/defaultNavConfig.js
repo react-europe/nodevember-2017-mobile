643ed51662baab2af83de313a68be83d0abc0ae1
m4gie <steeven.regnault@epitech.eu>
2020-04-03 17:33:55 +0200

    Navigation: Add file for DefaultStackConfig

---
+++ src/utils/defaultNavConfig.js
@@ -0,0 +1,21 @@
+import {Colors} from '../constants';
+
+const DefaultStackConfig = {
+  cardStyle: {
+    backgroundColor: '#fafafa',
+  },
+  options: {
+    title: 'Title',
+    headerStyle: {
+      borderBottomWidth: 0,
+      shadowRadius: 0,
+      backgroundColor: Colors.blue,
+    },
+    headerTitleStyle: {
+      headerTintColor: 'white',
+      fontFamily: 'open-sans-bold',
+    },
+  },
+};
+
+export default DefaultStackConfig;
