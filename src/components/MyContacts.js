65ef0b3d518f7ea047a02a1afdf90d6607e4b260
m4gie <steeven.regnault@epitech.eu>
2020-04-14 13:59:50 +0200

    Core: convert MyContact to functional component

--- src/components/MyContacts.js
+++ src/components/MyContacts.js
@@ -1,68 +1,18 @@
 import React from 'react';
-import {Alert, Clipboard, View, AsyncStorage, StyleSheet} from 'react-native';
-import {BorderlessButton, RectButton} from 'react-native-gesture-handler';
-import {sendEmail, openTwitter, getContactTwitter} from '../utils';
+import {Alert, Clipboard, View, StyleSheet} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
 
+import {getContactTwitter} from '../utils';
 import {SemiBoldText} from './StyledText';
 import ContactCard from './ContactCard';
 import {FontSizes, Colors} from '../constants';
 
-export default class MyContacts extends React.Component {
-  state = {
-    contacts: [],
-  };
-  async getContacts() {
-    try {
-      const value = await AsyncStorage.getItem('@MySuperStore2019:contacts');
-      this.setState({contacts: JSON.parse(value)});
-    } catch (err) {
-      return [];
-    }
-  }
+export default function MyContacts(props) {
+  const contacts = props.contacts;
 
-  constructor(props) {
-    super(props);
-    this.contacts = this.props.contacts;
-    this.tickets = this.props.tickets;
-    // console.log("tickets from MyContacts", this.tickets);
-  }
-  componentDidMount() {
-    this.getContacts();
-  }
-  render() {
-    const contacts = this.props.contacts;
-    return (
-      <View style={[{marginHorizontal: 10}, this.props.style]}>
-        <SemiBoldText style={{fontSize: FontSizes.title}}>
-          My Contacts
-        </SemiBoldText>
-        {contacts && contacts.length > 0 ? (
-          <ClipBorderRadius>
-            <RectButton
-              style={styles.bigButton}
-              onPress={this._handlePressCopyEmails}
-              underlayColor="#fff">
-              <SemiBoldText style={styles.bigButtonText}>
-                {'Copy all emails to clipboard'}
-              </SemiBoldText>
-            </RectButton>
-          </ClipBorderRadius>
-        ) : null}
-        {this.props.contacts.map(contact => (
-          <ContactCard
-            key={contact.id + contact.email}
-            contact={contact}
-            tickets={this.props.tickets}
-            style={{marginTop: 10, marginBottom: 10}}
-          />
-        ))}
-      </View>
-    );
-  }
-
-  _handlePressCopyEmails = () => {
+  const _handlePressCopyEmails = () => {
     let contacts = 'first name,last name,email,twitter\n';
-    this.props.contacts.map(contact => {
+    props.contacts.map(contact => {
       contacts +=
         contact.firstName +
         ',' +
@@ -80,6 +30,34 @@ export default class MyContacts extends React.Component {
       [{text: 'OK', onPress: () => Clipboard.setString(contacts)}]
     );
   };
+
+  return (
+    <View style={[{marginHorizontal: 10}, props.style]}>
+      <SemiBoldText style={{fontSize: FontSizes.title}}>
+        My Contacts
+      </SemiBoldText>
+      {contacts && contacts.length > 0 ? (
+        <ClipBorderRadius>
+          <RectButton
+            style={styles.bigButton}
+            onPress={_handlePressCopyEmails}
+            underlayColor="#fff">
+            <SemiBoldText style={styles.bigButtonText}>
+              {'Copy all emails to clipboard'}
+            </SemiBoldText>
+          </RectButton>
+        </ClipBorderRadius>
+      ) : null}
+      {contacts.map(contact => (
+        <ContactCard
+          key={contact.id + contact.email}
+          contact={contact}
+          tickets={props.tickets}
+          style={{marginTop: 10, marginBottom: 10}}
+        />
+      ))}
+    </View>
+  );
 }
 
 const ClipBorderRadius = ({children, style}) => {
