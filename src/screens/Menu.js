a3bc4c483f0f2533125c5128581fa3ab5134121f
m4gie <steeven.regnault@epitech.eu>
2020-04-14 11:08:18 +0200

    Core: convert Menu to functional component

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
   View,
   Image,
@@ -12,101 +12,93 @@ import {Ionicons} from '@expo/vector-icons';
 import {Colors, Layout} from '../constants';
 import CachedImage from '../components/CachedImage';
 
-class MenuScreen extends Component {
-  static navigationOptions = {
-    title: 'Menu',
-  };
-
-  getIconName = key => {
+function MenuScreen(props) {
+  function getIconName(key) {
     if (key === 'Speakers') return 'ios-microphone';
     if (key === 'Crew') return 'ios-information-circle';
     if (key === 'Sponsors') return 'ios-beer';
     if (key === 'Attendees') return 'ios-people';
-  };
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <StatusBar barStyle="light-content" />
-        <FlatList
-          data={[
-            {key: 'Speakers'},
-            {key: 'Crew'},
-            {key: 'Sponsors'},
-            {key: 'Attendees'},
-          ]}
-          ListHeaderComponent={() => (
+  }
+  return (
+    <View style={{flex: 1}}>
+      <StatusBar barStyle="light-content" />
+      <FlatList
+        data={[
+          {key: 'Speakers'},
+          {key: 'Crew'},
+          {key: 'Sponsors'},
+          {key: 'Attendees'},
+        ]}
+        ListHeaderComponent={() => (
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: 240 + Layout.notchHeight,
+            }}>
+            <CachedImage
+              source={require('../assets/hero.png')}
+              style={{
+                height: 240 + Layout.notchHeight,
+                width: Layout.window.width,
+                resizeMode: 'cover',
+                position: 'absolute',
+              }}
+            />
+            <View
+              style={[
+                StyleSheet.absoluteFill,
+                {
+                  backgroundColor: 'rgba(0,0,0,0.5)',
+                },
+              ]}
+            />
+            <Image
+              source={require('../assets/logo.png')}
+              style={[
+                {
+                  width: 220,
+                  height: 100,
+                  resizeMode: 'contain',
+                },
+              ]}
+            />
+          </View>
+        )}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              height: StyleSheet.hairlineWidth,
+              backgroundColor: '#cdcdcd',
+            }}
+          />
+        )}
+        renderItem={({item}) => (
+          <TouchableOpacity onPress={() => props.navigation.navigate(item.key)}>
             <View
               style={{
                 flex: 1,
+                paddingVertical: 12,
+                paddingHorizontal: 16,
+                flexDirection: 'row',
                 alignItems: 'center',
-                justifyContent: 'center',
-                height: 240 + Layout.notchHeight,
               }}>
-              <CachedImage
-                source={require('../assets/hero.png')}
-                style={{
-                  height: 240 + Layout.notchHeight,
-                  width: Layout.window.width,
-                  resizeMode: 'cover',
-                  position: 'absolute',
-                }}
+              <Ionicons
+                name={getIconName(item.key)}
+                size={24}
+                color={Colors.blue}
               />
-              <View
-                style={[
-                  StyleSheet.absoluteFill,
-                  {
-                    backgroundColor: 'rgba(0,0,0,0.5)',
-                  },
-                ]}
-              />
-              <Image
-                source={require('../assets/logo.png')}
-                style={[
-                  {
-                    width: 220,
-                    height: 100,
-                    resizeMode: 'contain',
-                  },
-                ]}
-              />
+              <Text style={{fontSize: 20, marginHorizontal: 16, flex: 1}}>
+                {item.key}
+              </Text>
+              <Ionicons name="ios-arrow-forward" size={24} color="#999" />
             </View>
-          )}
-          ItemSeparatorComponent={() => (
-            <View
-              style={{
-                height: StyleSheet.hairlineWidth,
-                backgroundColor: '#cdcdcd',
-              }}
-            />
-          )}
-          renderItem={({item}) => (
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate(item.key)}>
-              <View
-                style={{
-                  flex: 1,
-                  paddingVertical: 12,
-                  paddingHorizontal: 16,
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <Ionicons
-                  name={`${this.getIconName(item.key)}`}
-                  size={24}
-                  color={Colors.blue}
-                />
-                <Text style={{fontSize: 20, marginHorizontal: 16, flex: 1}}>
-                  {item.key}
-                </Text>
-                <Ionicons name="ios-arrow-forward" size={24} color="#999" />
-              </View>
-            </TouchableOpacity>
-          )}
-        />
-      </View>
-    );
-  }
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
 }
 
 export default MenuScreen;
