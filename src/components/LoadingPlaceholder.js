b446fdd8376839f12ca968d706a2c1530cc2380a
m4gie <steeven.regnault@epitech.eu>
2020-04-14 11:45:49 +0200

    Core: convert LoadingPlaceholder to FC

--- src/components/LoadingPlaceholder.js
+++ src/components/LoadingPlaceholder.js
@@ -1,44 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {ActivityIndicator, Platform, View} from 'react-native';
 import {Colors} from '../constants';
 
 // All this does is briefly render a loading indicator when you
 // first mount a component as a child of this component
-export default class LoadingPlaceholder extends React.Component {
-  state = {
-    isReady: false,
-  };
+export default function LoadingPlaceholder(props) {
+  const [isReady, setIsReady] = useState(false);
+  let timer = undefined;
 
-  componentDidMount() {
-    if (!this.state.isReady) {
-      this.timer = setTimeout(
+  useEffect(() => {
+    if (!isReady) {
+      timer = setTimeout(
         () => {
-          this.setState({isReady: true});
+          setIsReady(true);
         },
         Platform.OS === 'ios' ? 250 : 500
       );
     }
-  }
+    return function unmount() {
+      if (timer) {
+        clearTimeout(timer);
+        timer = 0;
+      }
+    };
+  }, []);
 
-  componentWillUnmount() {
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = 0;
-    }
-  }
-
-  render() {
-    if (!this.state.isReady) {
-      return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <ActivityIndicator
-            color={Platform.OS === 'android' ? Colors.blue : '#888'}
-            size="large"
-          />
-        </View>
-      );
-    } else {
-      return this.props.children;
-    }
+  if (!isReady) {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <ActivityIndicator
+          color={Platform.OS === 'android' ? Colors.blue : '#888'}
+          size="large"
+        />
+      </View>
+    );
+  } else {
+    return props.children;
   }
 }
