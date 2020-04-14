045ec9a569c7bf219cecf2a12477fbc4f8eae0d6
m4gie <steeven.regnault@epitech.eu>
2020-04-14 12:19:54 +0200

    Core: convert Attendees to functional component

--- src/screens/Attendees.js
+++ src/screens/Attendees.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
-  Animated,
   Platform,
   Text,
   StyleSheet,
@@ -15,71 +14,76 @@ import _ from 'lodash';
 
 import withNavigation from '../utils/withNavigation';
 import AttendeesSearchResults from '../components/AttendeesSearchResults';
-
 import {GQL} from '../constants';
 import {Colors, Layout} from '../constants';
 import GET_ATTENDEES from '../data/attendeesquery';
 import {getContactTwitter} from '../utils';
 import {withData} from '../context/DataContext';
 
-class Attendees extends React.Component {
-  static navigationOptions = {
-    title: 'Attendees',
-  };
+function Attendees(props) {
+  const [aquery, setAquery] = useState('');
 
-  state = {
-    scrollY: new Animated.Value(0),
-    attendees: [],
-    aquery: '',
-  };
-  constructor(props) {
-    super(props);
-  }
-  throttleDelayMs = 200;
-  throttleTimeout = null;
-  queryThrottle = text => {
-    clearTimeout(this.throttleTimeout);
+  const throttleDelayMs = 200;
+  let throttleTimeout = null;
+  const queryThrottle = text => {
+    if (throttleTimeout) {
+      clearTimeout(throttleTimeout);
+    }
 
-    this.throttleTimeout = setTimeout(() => {
+    throttleTimeout = setTimeout(() => {
       LayoutAnimation.easeInEaseOut();
-      this.setState({aquery: text});
-    }, this.throttleDelayMs);
+      setAquery(text);
+    }, throttleDelayMs);
   };
 
-  render() {
-    const {scrollY} = this.state;
-    return (
-      <View style={{flex: 1}}>
-        <Searchbar
-          onChangeText={text => this.queryThrottle(text)}
-          placeholder="Search for conference attendees"
-          style={styles.textInput}
-          autoCapitalize="none"
-          autoCorrect={false}
-          clearButtonMode="while-editing"
-        />
-        <DeferredAttendeesContentWithNavigation
-          aquery={this.state.aquery}
-          event={this.props.event}
-        />
-        <OverscrollView />
-      </View>
-    );
-  }
+  useState(() => {
+    return function unmount() {
+      if (throttleTimeout) {
+        clearTimeout(throttleTimeout);
+        throttleTimeout = 0;
+      }
+    };
+  });
+
+  return (
+    <View style={{flex: 1}}>
+      <Searchbar
+        onChangeText={text => queryThrottle(text)}
+        placeholder="Search for conference attendees"
+        style={styles.textInput}
+        autoCapitalize="none"
+        autoCorrect={false}
+        clearButtonMode="while-editing"
+      />
+      <DeferredAttendeesContentWithNavigation
+        aquery={aquery}
+        event={props.event}
+      />
+      <OverscrollView />
+    </View>
+  );
 }
 
-class DeferredAttendeesContent extends React.Component {
-  state = {
-    ready: Platform.OS === 'android' ? false : true,
-    uuid: '',
-  };
+function DeferredAttendeesContent(props) {
+  const [ready, setReady] = useState(Platform.OS === 'android' ? false : true);
+  const [uuid, setUuid] = useState('');
+  let timer = undefined;
 
-  constructor(props) {
-    super(props);
-    this._getUuid();
-  }
+  useEffect(() => {
+    _getUuid();
+    if (ready) {
+      return;
+    }
+    timer = setTimeout(() => setReady(true), 200);
+    return function unmount() {
+      if (timer) {
+        clearTimeout(timer);
+        timer = 0;
+      }
+    };
+  }, []);
 
-  async _getUuid() {
+  async function _getUuid() {
     const value = await AsyncStorage.getItem('@MySuperStore2019:tickets');
     let tickets = JSON.parse(value) || [];
     let uuid = '';
@@ -87,107 +91,97 @@ class DeferredAttendeesContent extends React.Component {
       ticket.checkinLists.map(ch => {
         if (ch.mainEvent) {
           uuid = ticket.uuid;
-          this.setState({uuid: uuid});
+          setUuid(uuid);
         }
       });
     });
   }
 
-  componentDidMount() {
-    if (this.state.ready) {
-      return;
-    }
-    setTimeout(() => this.setState({ready: true}), 200);
-  }
-
-  _handlePressRow = attendee => {
-    this.props.navigation.navigate('AttendeeDetail', {attendee});
+  const _handlePressRow = attendee => {
+    props.navigation.navigate('AttendeeDetail', {attendee});
   };
 
-  render() {
-    if (!this.state.ready) {
-      return null;
-    }
-    const {aquery} = this.props;
-    const cleanedQuery = aquery.toLowerCase().trim();
-    const uuid = this.state.uuid;
-    const vars = {slug: GQL.slug, q: 'a', uuid: uuid};
-    // console.log(GET_ATTENDEES, vars);
-    return (
-      <AnimatableView animation="fadeIn" useNativeDriver duration={800}>
-        {uuid != '' ? (
-          <Query query={GET_ATTENDEES} variables={vars}>
-            {({loading, error, data}) => {
-              if (error) {
-                return <Text>Error ${error}</Text>;
-              }
-
-              const attendees =
-                data && data.events && data.events[0]
-                  ? data.events[0].attendees
-                  : [];
-              let attendeesData;
-              if (cleanedQuery === '') {
-                attendeesData = _.orderBy(
-                  attendees,
-                  attendee => `${attendee.firstName} ${attendee.lastName}`,
-                  ['asc']
-                );
-              } else {
-                const filteredAttendees = [];
-                const attendeesSearchRankingScore = {};
-                attendees.forEach(attendee => {
-                  const fullName = `${attendee.firstName} ${attendee.lastName}`;
-                  const matchesName = fullName
-                    .toLowerCase()
-                    .trim()
-                    .includes(cleanedQuery);
-                  const matchesEmail = attendee.email
-                    .toLowerCase()
-                    .trim()
-                    .includes(cleanedQuery);
-                  const matchesTwitter = getContactTwitter(attendee)
-                    .toLowerCase()
-                    .trim()
-                    .includes(cleanedQuery);
-
-                  attendeesSearchRankingScore[`${attendee.id}`] = 0;
-                  if (matchesName || matchesEmail || matchesTwitter) {
-                    filteredAttendees.push(attendee);
-                  }
-                  if (matchesName) {
-                    attendeesSearchRankingScore[`${attendee.id}`] += 1;
-                  }
-                  if (matchesEmail) {
-                    attendeesSearchRankingScore[`${attendee.id}`] += 1;
-                  }
-                  if (matchesTwitter) {
-                    attendeesSearchRankingScore[`${attendee.id}`] += 1;
-                  }
-                });
-                const sortedFilteredAttendees = _.orderBy(
-                  filteredAttendees,
-                  attendee => attendeesSearchRankingScore[`${attendee.id}`],
-                  ['desc']
-                );
-                attendeesData = sortedFilteredAttendees;
-              }
-
-              return (
-                <AttendeesSearchResults
-                  attendees={attendeesData}
-                  onPress={this._handlePressRow}
-                  searchQuery={cleanedQuery}
-                  isLoading={loading}
-                  event={this.props.event}
-                />
-              );
-            }}
-          </Query>
-        ) : null}
-      </AnimatableView>
-    );
+  if (!ready) {
+    return null;
   }
+  const {aquery} = props;
+  const cleanedQuery = aquery.toLowerCase().trim();
+  const vars = {slug: GQL.slug, q: 'a', uuid: uuid};
+  // console.log(GET_ATTENDEES, vars);
+  return (
+    <AnimatableView animation="fadeIn" useNativeDriver duration={800}>
+      {uuid != '' ? (
+        <Query query={GET_ATTENDEES} variables={vars}>
+          {({loading, error, data}) => {
+            if (error) {
+              return <Text>Error ${error}</Text>;
+            }
+
+            const attendees =
+              data && data.events && data.events[0]
+                ? data.events[0].attendees
+                : [];
+            let attendeesData;
+            if (cleanedQuery === '') {
+              attendeesData = _.orderBy(
+                attendees,
+                attendee => `${attendee.firstName} ${attendee.lastName}`,
+                ['asc']
+              );
+            } else {
+              const filteredAttendees = [];
+              const attendeesSearchRankingScore = {};
+              attendees.forEach(attendee => {
+                const fullName = `${attendee.firstName} ${attendee.lastName}`;
+                const matchesName = fullName
+                  .toLowerCase()
+                  .trim()
+                  .includes(cleanedQuery);
+                const matchesEmail = attendee.email
+                  .toLowerCase()
+                  .trim()
+                  .includes(cleanedQuery);
+                const matchesTwitter = getContactTwitter(attendee)
+                  .toLowerCase()
+                  .trim()
+                  .includes(cleanedQuery);
+
+                attendeesSearchRankingScore[`${attendee.id}`] = 0;
+                if (matchesName || matchesEmail || matchesTwitter) {
+                  filteredAttendees.push(attendee);
+                }
+                if (matchesName) {
+                  attendeesSearchRankingScore[`${attendee.id}`] += 1;
+                }
+                if (matchesEmail) {
+                  attendeesSearchRankingScore[`${attendee.id}`] += 1;
+                }
+                if (matchesTwitter) {
+                  attendeesSearchRankingScore[`${attendee.id}`] += 1;
+                }
+              });
+              const sortedFilteredAttendees = _.orderBy(
+                filteredAttendees,
+                attendee => attendeesSearchRankingScore[`${attendee.id}`],
+                ['desc']
+              );
+              attendeesData = sortedFilteredAttendees;
+            }
+
+            return (
+              <AttendeesSearchResults
+                attendees={attendeesData}
+                onPress={_handlePressRow}
+                searchQuery={cleanedQuery}
+                isLoading={loading}
+                event={props.event}
+              />
+            );
+          }}
+        </Query>
+      ) : null}
+    </AnimatableView>
+  );
 }
 
 const DeferredAttendeesContentWithNavigation = withNavigation(
