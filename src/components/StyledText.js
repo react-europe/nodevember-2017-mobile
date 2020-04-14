2132fd607886d647e8ae15eb41aca11d83e18918
m4gie <steeven.regnault@epitech.eu>
2020-04-14 11:39:47 +0200

    Core: convert StyledText to functional component

--- src/components/StyledText.js
+++ src/components/StyledText.js
@@ -1,47 +1,41 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-export class RegularText extends React.Component {
-  render() {
-    return (
-      <Text
-        {...this.props}
-        style={[
-          {backgroundColor: 'transparent'},
-          this.props.style,
-          {fontFamily: 'open-sans'},
-        ]}
-      />
-    );
-  }
+export function RegularText(props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        {backgroundColor: 'transparent'},
+        props.style,
+        {fontFamily: 'open-sans'},
+      ]}
+    />
+  );
 }
 
-export class SemiBoldText extends React.Component {
-  render() {
-    return (
-      <Text
-        {...this.props}
-        style={[
-          {backgroundColor: 'transparent'},
-          this.props.style,
-          {fontFamily: 'open-sans-semibold'},
-        ]}
-      />
-    );
-  }
+export function SemiBoldText(props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        {backgroundColor: 'transparent'},
+        props.style,
+        {fontFamily: 'open-sans-semibold'},
+      ]}
+    />
+  );
 }
 
-export class BoldText extends React.Component {
-  render() {
-    return (
-      <Text
-        {...this.props}
-        style={[
-          {backgroundColor: 'transparent'},
-          this.props.style,
-          {fontFamily: 'open-sans-bold'},
-        ]}
-      />
-    );
-  }
+export function BoldText(props) {
+  return (
+    <Text
+      {...props}
+      style={[
+        {backgroundColor: 'transparent'},
+        props.style,
+        {fontFamily: 'open-sans-bold'},
+      ]}
+    />
+  );
 }
