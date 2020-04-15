e174c2ef4d6828e43a1905cc33c0666ea942c90b
m4gie <steeven.regnault@epitech.eu>
2020-04-15 16:24:36 +0200

    Core: convert DynamicSchedule to FC

--- src/navigation/DynamicScheduleNavigation.js
+++ src/navigation/DynamicScheduleNavigation.js
@@ -7,32 +7,30 @@ import {withData} from '../context/DataContext';
 
 const Tab = createMaterialTopTabNavigator();
 
-class DynamicScheduleNavigation extends React.Component {
-  render() {
-    const fullSchedule = this.props.event.groupedSchedule;
-    return (
-      <Tab.Navigator
-        tabBarOptions={{
-          style: {backgroundColor: '#333'},
-          activeTintColor: '#fff',
-        }}
-        screenOptions={({route}) => ({
-          tabBarLabel: route.name.substring(0, 3).toUpperCase(),
-        })}>
-        {fullSchedule.map(day => (
-          <Tab.Screen
-            key={day.title}
-            name={day.title}
-            component={Screens.ScheduleDay}
-            initialParams={{
-              day: day.title,
-              date: moment(new Date(day.date)).format('ddd'),
-            }}
-          />
-        ))}
-      </Tab.Navigator>
-    );
-  }
+function DynamicScheduleNavigation(props) {
+  const fullSchedule = props.event.groupedSchedule;
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        style: {backgroundColor: '#333'},
+        activeTintColor: '#fff',
+      }}
+      screenOptions={({route}) => ({
+        tabBarLabel: route.name.substring(0, 3).toUpperCase(),
+      })}>
+      {fullSchedule.map(day => (
+        <Tab.Screen
+          key={day.title}
+          name={day.title}
+          component={Screens.ScheduleDay}
+          initialParams={{
+            day: day.title,
+            date: moment(new Date(day.date)).format('ddd'),
+          }}
+        />
+      ))}
+    </Tab.Navigator>
+  );
 }
 
 export default withData(DynamicScheduleNavigation);
