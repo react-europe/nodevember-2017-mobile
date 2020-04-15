3aaf03a8a7ad4059f14f589637eca49ab01102dc
m4gie <steeven.regnault@epitech.eu>
2020-04-15 10:12:32 +0200

    Core: convert AttendeeDetail to FC

--- src/screens/AttendeeDetail.js
+++ src/screens/AttendeeDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Animated,
   Platform,
@@ -17,156 +17,145 @@ import NavigationBar from '../components/NavigationBar';
 import {Colors, FontSizes, Layout} from '../constants';
 import {RegularText, SemiBoldText} from '../components/StyledText';
 import GravatarImage from '../components/GravatarImage';
-import CloseButton from '../components/CloseButton';
-import {sendEmail, openTwitter, addContact, getContactTwitter} from '../utils';
+import {openTwitter, addContact, getContactTwitter} from '../utils';
 import withHeaderHeight from '../utils/withHeaderHeight';
 
-class AttendeeDetail extends React.Component {
-  static navigationOptions = {
-    title: 'Attendee Details',
-  };
+function AttendeeDetail(props) {
+  const [scrollY] = useState(new Animated.Value(0));
 
-  state = {
-    scrollY: new Animated.Value(0),
-  };
-
-  _handlePressTwitter = () => {
-    const {attendee} = this.props.navigation.state.params;
+  const _handlePressTwitter = () => {
+    const {attendee} = props.route.params;
     const twitter = getContactTwitter(attendee);
     openTwitter(twitter);
   };
 
-  _handlePressEmail = () => {
+  /* const _handlePressEmail = () => {
     const {
       attendee: {email: emailTo},
-    } = this.props.navigation.state.params;
-    const {tickets} = this.props;
+    } = props.route.params;
+    const {tickets} = props;
     let fromName = {firstName: '', lastName: ''};
     if (tickets && tickets[0] && tickets[0].firstName) {
       fromName = tickets[0];
     }
     sendEmail(emailTo, fromName);
-  };
+  }; */
 
-  _handleAddToContacts = async () => {
-    const {navigation} = this.props;
-    const {attendee} = navigation.state.params;
+  const _handleAddToContacts = async () => {
+    const {attendee} = props.route.params;
     await addContact(attendee);
-    navigation.navigate('Contacts');
+    props.navigation.navigate('Contacts');
   };
 
-  render() {
-    const params = this.props.navigation.state.params || {};
-    const attendee = params.attendee;
+  const params = props.route.params || {};
+  const attendee = params.attendee;
 
-    if (!attendee || !_.has(attendee, 'email')) {
-      this.props.navigation.goBack();
-      return null;
-    }
+  if (!attendee || !_.has(attendee, 'email')) {
+    props.navigation.goBack();
+    return null;
+  }
 
-    const twitter = getContactTwitter(attendee);
+  const twitter = getContactTwitter(attendee);
 
-    const {scrollY} = this.state;
-    const scale = scrollY.interpolate({
-      inputRange: [-300, 0, 1],
-      outputRange: [2, 1, 1],
-      extrapolate: 'clamp',
-    });
-    const translateX = 0;
-    const translateY = scrollY.interpolate({
-      inputRange: [-300, 0, 1],
-      outputRange: [-50, 1, 1],
-      extrapolate: 'clamp',
-    });
+  const scale = scrollY.interpolate({
+    inputRange: [-300, 0, 1],
+    outputRange: [2, 1, 1],
+    extrapolate: 'clamp',
+  });
+  const translateX = 0;
+  const translateY = scrollY.interpolate({
+    inputRange: [-300, 0, 1],
+    outputRange: [-50, 1, 1],
+    extrapolate: 'clamp',
+  });
 
-    const headerOpacity = scrollY.interpolate({
-      inputRange: [0, 30, 200],
-      outputRange: [0, 0, 1],
-    });
+  const headerOpacity = scrollY.interpolate({
+    inputRange: [0, 30, 200],
+    outputRange: [0, 0, 1],
+  });
 
-    return (
-      <View style={{flex: 1, backgroundColor: '#fff', overflow: 'hidden'}}>
-        {Platform.OS === 'ios' ? (
-          <Animated.View
-            style={{
-              position: 'absolute',
-              top: -350,
-              left: 0,
-              right: 0,
-              height: 400,
-              transform: [
-                {
-                  translateY: scrollY.interpolate({
-                    inputRange: [-1, 0, 1],
-                    outputRange: [1, 0, 0],
-                  }),
-                },
-              ],
-              backgroundColor: Colors.blue,
-            }}
-          />
-        ) : null}
-        <AnimatedScrollView
-          style={{flex: 1, backgroundColor: 'transparent'}}
-          scrollEventThrottle={1}
-          onScroll={Animated.event(
-            [
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff', overflow: 'hidden'}}>
+      {Platform.OS === 'ios' ? (
+        <Animated.View
+          style={{
+            position: 'absolute',
+            top: -350,
+            left: 0,
+            right: 0,
+            height: 400,
+            transform: [
               {
-                nativeEvent: {contentOffset: {y: this.state.scrollY}},
+                translateY: scrollY.interpolate({
+                  inputRange: [-1, 0, 1],
+                  outputRange: [1, 0, 0],
+                }),
               },
             ],
-            {useNativeDriver: true}
-          )}>
-          <View style={styles.headerContainer}>
-            <Animated.View
-              style={{transform: [{scale}, {translateX}, {translateY}]}}>
-              <FadeIn placeholderStyle={{backgroundColor: 'transparent'}}>
-                <GravatarImage style={styles.avatar} email={attendee.email} />
-              </FadeIn>
-            </Animated.View>
-            <SemiBoldText style={styles.headerText} key={attendee.id}>
-              {attendee.firstName} {attendee.lastName}
-            </SemiBoldText>
-            {twitter ? (
-              <TouchableOpacity onPress={this._handlePressTwitter}>
-                <RegularText style={styles.headerText}>@{twitter}</RegularText>
-              </TouchableOpacity>
-            ) : null}
-          </View>
-          <AnimatableView
-            animation="fadeIn"
-            useNativeDriver
-            delay={50}
-            duration={250}
-            style={styles.content}>
-            <View>
-              <Button raised primary onPress={this._handleAddToContacts}>
-                Add to contacts
-              </Button>
-            </View>
-          </AnimatableView>
-        </AnimatedScrollView>
-
-        <NavigationBar
-          animatedBackgroundOpacity={headerOpacity}
-          style={[
-            Platform.OS === 'android'
-              ? {height: this.props.headerHeight + Constants.statusBarHeight}
-              : null,
-          ]}
-          renderLeftButton={() => (
-            <View
-              style={{
-                // gross dumb things
-                paddingTop: Platform.OS === 'android' ? 30 : 0,
-                marginTop: Layout.notchHeight > 0 ? -5 : 0,
-              }}
-            />
-          )}
+            backgroundColor: Colors.blue,
+          }}
         />
-      </View>
-    );
-  }
+      ) : null}
+      <AnimatedScrollView
+        style={{flex: 1, backgroundColor: 'transparent'}}
+        scrollEventThrottle={1}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {contentOffset: {y: scrollY}},
+            },
+          ],
+          {useNativeDriver: true}
+        )}>
+        <View style={styles.headerContainer}>
+          <Animated.View
+            style={{transform: [{scale}, {translateX}, {translateY}]}}>
+            <FadeIn placeholderStyle={{backgroundColor: 'transparent'}}>
+              <GravatarImage style={styles.avatar} email={attendee.email} />
+            </FadeIn>
+          </Animated.View>
+          <SemiBoldText style={styles.headerText} key={attendee.id}>
+            {attendee.firstName} {attendee.lastName}
+          </SemiBoldText>
+          {twitter ? (
+            <TouchableOpacity onPress={_handlePressTwitter}>
+              <RegularText style={styles.headerText}>@{twitter}</RegularText>
+            </TouchableOpacity>
+          ) : null}
+        </View>
+        <AnimatableView
+          animation="fadeIn"
+          useNativeDriver
+          delay={50}
+          duration={250}
+          style={styles.content}>
+          <View>
+            <Button raised primary onPress={_handleAddToContacts}>
+              Add to contacts
+            </Button>
+          </View>
+        </AnimatableView>
+      </AnimatedScrollView>
+
+      <NavigationBar
+        animatedBackgroundOpacity={headerOpacity}
+        style={[
+          Platform.OS === 'android'
+            ? {height: props.headerHeight + Constants.statusBarHeight}
+            : null,
+        ]}
+        renderLeftButton={() => (
+          <View
+            style={{
+              // gross dumb things
+              paddingTop: Platform.OS === 'android' ? 30 : 0,
+              marginTop: Layout.notchHeight > 0 ? -5 : 0,
+            }}
+          />
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
