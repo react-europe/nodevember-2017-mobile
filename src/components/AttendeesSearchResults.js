31db82b998a7b711d25e983d59b9f3c13ae7da4d
m4gie <steeven.regnault@epitech.eu>
2020-04-15 14:47:25 +0200

    Core: convert AttendeesSearchResult to FC

--- src/components/AttendeesSearchResults.js
+++ src/components/AttendeesSearchResults.js
@@ -15,80 +15,67 @@ import HighlightableText from './HighlightableText';
 
 import {getContactTwitter} from '../utils';
 
-class AttendeesSearchResultRow extends React.Component {
-  constructor(props) {
-    super(props);
-    const event = this.props.event;
-    const SpeakersAndTalks = event && evnt.speakers ? event.speakers : [];
-    this.SpeakersData = [{data: SpeakersAndTalks, title: 'Speakers'}];
+function AttendeesSearchResultRow(props) {
+  const event = props.event;
+  const SpeakersAndTalks = event && event.speakers ? event.speakers : [];
+  const SpeakersData = [{data: SpeakersAndTalks, title: 'Speakers'}];
+  const {attendee, searchQuery} = props;
+
+  let isSpeaker;
+  if (SpeakersData && SpeakersData.length > 0) {
+    isSpeaker = SpeakersData[0].data.filter(speaker => {
+      return getContactTwitter(attendee) === speaker.twitter;
+    })[0]
+      ? true
+      : false;
   }
 
-  render() {
-    const {attendee, searchQuery} = this.props;
-
-    let isSpeaker;
-    if (
-      this.SpeakersData &&
-      this.SpeakersData.length &&
-      this.SpeakersData.length > 0
-    ) {
-      isSpeaker = this.SpeakersData[0].data.filter(speaker => {
-        return getContactTwitter(attendee) === speaker.twitter;
-      })[0]
-        ? true
-        : false;
-    }
-
-    return (
-      <RectButton
-        onPress={() => this.props.onPress(attendee)}
-        activeOpacity={0.05}
-        style={{flex: 1, backgroundColor: '#fff'}}>
-        <View style={[styles.row, {borderLeftWidth: isSpeaker ? 5 : 0}]}>
-          {isSpeaker && (
-            <View style={styles.micIcon}>
-              <MaterialCommunityIcons
-                name="presentation"
-                size={32}
-                color="#aab8c2"
+  return (
+    <RectButton
+      onPress={() => props.onPress(attendee)}
+      activeOpacity={0.05}
+      style={{flex: 1, backgroundColor: '#fff'}}>
+      <View style={[styles.row, {borderLeftWidth: isSpeaker ? 5 : 0}]}>
+        {isSpeaker && (
+          <View style={styles.micIcon}>
+            <MaterialCommunityIcons
+              name="presentation"
+              size={32}
+              color="#aab8c2"
+            />
+          </View>
+        )}
+        <View
+          style={[styles.rowAvatarContainer, {paddingLeft: isSpeaker ? 0 : 5}]}>
+          <GravatarImage style={styles.avatarImage} email={attendee.email} />
+        </View>
+        <View style={styles.rowData}>
+          <HighlightableText
+            TextComponent={BoldText}
+            highlightStyle={{backgroundColor: '#e1e8ed'}}
+            searchWords={[searchQuery]}
+            textToHighlight={`${attendee.firstName} ${attendee.lastName}`}
+          />
+          {getContactTwitter(attendee) ? (
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Entypo
+                name={'twitter'}
+                size={16}
+                color="#1da1f2"
+                style={{paddingRight: 3}}
+              />
+              <HighlightableText
+                TextComponent={RegularText}
+                highlightStyle={{backgroundColor: '#e1e8ed'}}
+                searchWords={[searchQuery]}
+                textToHighlight={`@${getContactTwitter(attendee)}`}
               />
             </View>
-          )}
-          <View
-            style={[
-              styles.rowAvatarContainer,
-              {paddingLeft: isSpeaker ? 0 : 5},
-            ]}>
-            <GravatarImage style={styles.avatarImage} email={attendee.email} />
-          </View>
-          <View style={styles.rowData}>
-            <HighlightableText
-              TextComponent={BoldText}
-              highlightStyle={{backgroundColor: '#e1e8ed'}}
-              searchWords={[searchQuery]}
-              textToHighlight={`${attendee.firstName} ${attendee.lastName}`}
-            />
-            {getContactTwitter(attendee) ? (
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Entypo
-                  name={'twitter'}
-                  size={16}
-                  color="#1da1f2"
-                  style={{paddingRight: 3}}
-                />
-                <HighlightableText
-                  TextComponent={RegularText}
-                  highlightStyle={{backgroundColor: '#e1e8ed'}}
-                  searchWords={[searchQuery]}
-                  textToHighlight={`@${getContactTwitter(attendee)}`}
-                />
-              </View>
-            ) : null}
-          </View>
+          ) : null}
         </View>
-      </RectButton>
-    );
-  }
+      </View>
+    </RectButton>
+  );
 }
 
 const AttendeesSearchResultPlaceholderRow = () => (
@@ -119,44 +106,31 @@ const AttendeesSearchResultPlaceholderRow = () => (
   </RectButton>
 );
 
-export default class AttendeesSearchResults extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const {attendees, isLoading} = this.props;
-    const maybeAttendees = isLoading
-      ? _.times(10).map(id => ({id}))
-      : attendees;
-    const itemRenderer = isLoading
-      ? this._renderItemPlaceholder
-      : this._renderItem;
-    return (
-      <FlatList
-        renderScrollComponent={props => <ScrollView {...props} />}
-        renderItem={itemRenderer}
-        data={maybeAttendees}
-        keyExtractor={item => `${item.id}`}
-        initialNumToRender={10}
-        keyboardDismissMode="on-drag"
-        style={styles.list}
-      />
-    );
-  }
+export default function AttendeesSearchResults(props) {
+  const {attendees, isLoading} = props;
 
-  _renderItem = ({item: attendee}) => (
+  const _renderItem = ({item: attendee}) => (
     <AttendeesSearchResultRow
       attendee={attendee}
-      onPress={this.props.onPress}
-      searchQuery={this.props.searchQuery}
+      onPress={props.onPress}
+      searchQuery={props.searchQuery}
     />
   );
 
-  _renderItemPlaceholder = () => <AttendeesSearchResultPlaceholderRow />;
-
-  _handlePressRow = attendee => {
-    this.props.navigation.navigate('AttendeeDetail', {attendee});
-  };
+  const _renderItemPlaceholder = () => <AttendeesSearchResultPlaceholderRow />;
+  const maybeAttendees = isLoading ? _.times(10).map(id => ({id})) : attendees;
+  const itemRenderer = isLoading ? _renderItemPlaceholder : _renderItem;
+  return (
+    <FlatList
+      renderScrollComponent={props => <ScrollView {...props} />}
+      renderItem={itemRenderer}
+      data={maybeAttendees}
+      keyExtractor={item => `${item.id}`}
+      initialNumToRender={10}
+      keyboardDismissMode="on-drag"
+      style={styles.list}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
