b98598c20bc9cbc93178768b08e5ea1a21820bd1
m4gie <steeven.regnault@epitech.eu>
2020-04-15 14:13:02 +0200

    Core: convert MenuButton to functional component

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import {Platform} from 'react-native';
-import {connectDrawerButton} from '../Navigation';
 import {BorderlessButton} from 'react-native-gesture-handler';
 import {Ionicons} from '@expo/vector-icons';
 import {Layout} from '../constants';
 
-export default class MenuButton extends React.Component {
-  render() {
-    return (
-      <BorderlessButton
-        onPress={this.props.openDrawer}
-        style={{
-          marginBottom: 2,
-          marginRight: 5,
-          marginTop: Layout.notchHeight > 0 ? 0 : 3,
-          marginLeft: 5,
-          paddingHorizontal: 10,
-          paddingVertical: 5,
-          alignSelf: 'flex-start',
-        }}
-        hitSlop={{left: 30, top: 30, right: 30, bottom: 30}}>
-        <Ionicons
-          name={Platform.OS === 'ios' ? 'ios-menu-outline' : 'md-menu'}
-          size={28}
-          color="#fff"
-        />
-      </BorderlessButton>
-    );
-  }
+export function MenuButton(props) {
+  return (
+    <BorderlessButton
+      onPress={props.openDrawer}
+      style={{
+        marginBottom: 2,
+        marginRight: 5,
+        marginTop: Layout.notchHeight > 0 ? 0 : 3,
+        marginLeft: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        alignSelf: 'flex-start',
+      }}
+      hitSlop={{left: 30, top: 30, right: 30, bottom: 30}}>
+      <Ionicons
+        name={Platform.OS === 'ios' ? 'ios-menu-outline' : 'md-menu'}
+        size={28}
+        color="#fff"
+      />
+    </BorderlessButton>
+  );
 }
