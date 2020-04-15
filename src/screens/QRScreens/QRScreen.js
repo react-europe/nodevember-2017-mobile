688becc7e8f46b7bc1ce89c7c4e8a54a24fb24b5
m4gie <steeven.regnault@epitech.eu>
2020-04-15 10:48:18 +0200

    Core: convert QRScreen to FC

--- src/screens/QRScreens/QRScreen.js
+++ src/screens/QRScreens/QRScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   ActivityIndicator,
   Button,
@@ -11,95 +11,91 @@ import {
 import * as Permissions from 'expo-permissions';
 import {BarCodeScanner} from 'expo-barcode-scanner';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useFocusEffect} from '@react-navigation/native';
 
 import withNavigation from '../../utils/withNavigation';
 
-class QRScreen extends React.Component {
-  state = {
-    showQRScanner: false,
-    hasCameraPermission: null,
-  };
+function QRScreen(props) {
+  const [showQRScanner, setShowQRScanner] = useState(false);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
 
-  componentDidMount() {
-    this._requestCameraPermission();
-    InteractionManager.runAfterInteractions(() => {
-      this.setState({showQRScanner: true});
-    });
-  }
+  useFocusEffect(
+    React.useCallback(() => {
+      _requestCameraPermission();
+      InteractionManager.runAfterInteractions(() => {
+        setShowQRScanner(true);
+      });
+    }, [])
+  );
 
-  _requestCameraPermission = async () => {
+  const _requestCameraPermission = async () => {
     const {status} = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted',
-    });
+    setHasCameraPermission(status === 'granted');
   };
 
-  _onBarCodeRead = data => {
-    this.setState({showQRScanner: false}, () => {
-      this.props.onBarCodeScanned(data);
-    });
+  const _onBarCodeRead = data => {
+    setShowQRScanner(false);
+    props.onBarCodeScanned(data);
   };
 
-  render() {
-    return (
-      <View style={{flex: 1, backgroundColor: 'black'}}>
-        {this.state.showQRScanner && this.state.hasCameraPermission ? (
-          <BarCodeScanner
-            onBarCodeScanned={this.props.loading ? null : this._onBarCodeRead}
-            style={{flex: 1}}
-          />
-        ) : null}
+  return (
+    <View style={{flex: 1, backgroundColor: 'black'}}>
+      {showQRScanner && hasCameraPermission ? (
+        <BarCodeScanner
+          onBarCodeScanned={props.loading ? null : _onBarCodeRead}
+          style={{flex: 1}}
+        />
+      ) : null}
 
-        <SafeAreaView
-          forceInset={{top: 'always'}}
-          style={{position: 'absolute', top: 0, left: 0, right: 0}}>
-          <Text
-            style={{
-              fontSize: 20,
-              marginTop: Platform.OS === 'ios' ? 15 : 40,
-              textAlign: 'center',
-              color: '#fff',
-            }}>
-            {this.props.title}
-          </Text>
-        </SafeAreaView>
-
-        <SafeAreaView
-          forceInset={{bottom: 'always'}}
+      <SafeAreaView
+        forceInset={{top: 'always'}}
+        style={{position: 'absolute', top: 0, left: 0, right: 0}}>
+        <Text
           style={{
-            position: 'absolute',
-            bottom: 10,
-            paddingBottom: Platform.OS === 'ios' ? 0 : 15,
-            left: 0,
-            right: 0,
-            alignItems: 'center',
+            fontSize: 20,
+            marginTop: Platform.OS === 'ios' ? 15 : 40,
+            textAlign: 'center',
+            color: '#fff',
           }}>
-          <Button
-            onPress={() => this.props.navigation.goBack()}
-            color={Platform.OS === 'ios' ? '#fff' : null}
-            title="Dismiss"
+          {props.title}
+        </Text>
+      </SafeAreaView>
+
+      <SafeAreaView
+        forceInset={{bottom: 'always'}}
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          paddingBottom: Platform.OS === 'ios' ? 0 : 15,
+          left: 0,
+          right: 0,
+          alignItems: 'center',
+        }}>
+        <Button
+          onPress={() => props.navigation.goBack()}
+          color={Platform.OS === 'ios' ? '#fff' : null}
+          title="Dismiss"
+        />
+      </SafeAreaView>
+      {props.loading ? (
+        <View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'transparent',
+            },
+          ]}>
+          <ActivityIndicator
+            color="#fff"
+            size="large"
+            style={{backgroundColor: 'transparent'}}
           />
-        </SafeAreaView>
-        {this.props.loading ? (
-          <View
-            style={[
-              StyleSheet.absoluteFill,
-              {
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'transparent',
-              },
-            ]}>
-            <ActivityIndicator
-              color="#fff"
-              size="large"
-              style={{backgroundColor: 'transparent'}}
-            />
-          </View>
-        ) : null}
-      </View>
-    );
-  }
+        </View>
+      ) : null}
+    </View>
+  );
 }
 
 export default withNavigation(QRScreen);
