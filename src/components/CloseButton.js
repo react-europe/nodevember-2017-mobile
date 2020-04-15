e63469501ffefa5c17934f0d17afae80206733fb
m4gie <steeven.regnault@epitech.eu>
2020-04-15 14:35:55 +0200

    Core: convert CloseButton to functional component

--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -4,41 +4,37 @@ import {View, Platform, StyleSheet} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 import {BorderlessButton} from 'react-native-gesture-handler';
 
-class CloseButton extends React.PureComponent {
-  static defaultProps = {
-    pressColorAndroid: 'rgba(0, 0, 0, .32)',
-    tintColor: Platform.select({
-      ios: '#037aff',
-    }),
-  };
+CloseButton.defaultProps = {
+  pressColorAndroid: 'rgba(0, 0, 0, .32)',
+  tintColor: Platform.select({
+    ios: '#037aff',
+  }),
+};
 
-  state = {};
+function CloseButton(props) {
+  const {onPress, pressColorAndroid} = props;
 
-  render() {
-    const {onPress, pressColorAndroid} = this.props;
-
-    return (
-      <BorderlessButton
-        accessibilityComponentType="button"
-        accessibilityLabel="Close details"
-        accessibilityTraits="button"
-        testID="header-back"
-        delayPressIn={0}
-        onPress={onPress}
-        pressColor={pressColorAndroid}
-        style={styles.container}
-        borderless>
-        <View style={styles.container}>
-          <Ionicons
-            name={Platform.OS === 'ios' ? 'ios-close' : 'md-close'}
-            size={Platform.OS === 'ios' ? 40 : 30}
-            color="#fff"
-            style={styles.icon}
-          />
-        </View>
-      </BorderlessButton>
-    );
-  }
+  return (
+    <BorderlessButton
+      accessibilityComponentType="button"
+      accessibilityLabel="Close details"
+      accessibilityTraits="button"
+      testID="header-back"
+      delayPressIn={0}
+      onPress={onPress}
+      pressColor={pressColorAndroid}
+      style={styles.container}
+      borderless>
+      <View style={styles.container}>
+        <Ionicons
+          name={Platform.OS === 'ios' ? 'ios-close' : 'md-close'}
+          size={Platform.OS === 'ios' ? 40 : 30}
+          color="#fff"
+          style={styles.icon}
+        />
+      </View>
+    </BorderlessButton>
+  );
 }
 
 const styles = StyleSheet.create({
