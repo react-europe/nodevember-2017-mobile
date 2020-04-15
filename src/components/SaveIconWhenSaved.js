f0ab81c31c971e90607f2a8ad74b95010bae9430
m4gie <steeven.regnault@epitech.eu>
2020-04-15 10:11:33 +0200

    Core: convert SaveIcon to functional component

--- src/components/SaveIconWhenSaved.js
+++ src/components/SaveIconWhenSaved.js
@@ -4,27 +4,25 @@ import {Ionicons} from '@expo/vector-icons';
 import {withSaveState} from '../utils/storage';
 import {Icons, Colors} from '../constants';
 
-class SaveIconWhenSaved extends React.Component {
-  render() {
-    if (!this.props.saved) {
-      return null;
-    }
-
-    const icon = (
-      <Ionicons
-        name={Icons.favoriteActive}
-        color={Colors.blue}
-        style={[{backgroundColor: 'transparent'}, this.props.style]}
-      />
-    );
-
-    return (
-      <Text>
-        {icon}
-        {'  '}
-      </Text>
-    );
+function SaveIconWhenSaved(props) {
+  if (!props.saved) {
+    return null;
   }
+
+  const icon = (
+    <Ionicons
+      name={Icons.favoriteActive}
+      color={Colors.blue}
+      style={[{backgroundColor: 'transparent'}, props.style]}
+    />
+  );
+
+  return (
+    <Text>
+      {icon}
+      {'  '}
+    </Text>
+  );
 }
 
 export default withSaveState(SaveIconWhenSaved);
