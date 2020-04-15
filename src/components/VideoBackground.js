fe9a279e6d82d9eb08499de77ffd2f0f16ca8823
m4gie <steeven.regnault@epitech.eu>
2020-04-15 12:35:08 +0200

    Core: convert VideoBackground to FC

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
-import { Video } from 'expo-av';
-import { Asset } from 'expo-asset';
+import {Video} from 'expo-av';
+import {Asset} from 'expo-asset';
 import {View as AnimatableView} from 'react-native-animatable';
 
-export default class VideoBackground extends React.Component {
-  state = {
-    videoLoaded: false,
-  };
+export default function VideoBackground() {
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
-  async componentDidMount() {
-    if (!this.state.videoLoaded) {
+  async function loadVideo() {
+    if (videoLoaded) {
       try {
         await Asset.fromModule(require('../assets/video.mp4')).downloadAsync();
-        this.setState({videoLoaded: true});
+        setVideoLoaded(true);
       } catch (e) {
         // Not working, oh well, no video for you
       }
     }
   }
 
-  render() {
-    if (this.state.videoLoaded) {
-      return (
-        <AnimatableView
-          animation="fadeIn"
+  useEffect(() => {
+    loadVideo();
+  }, []);
+
+  if (videoLoaded) {
+    return (
+      <AnimatableView
+        animation="fadeIn"
+        style={{flex: 1}}
+        useNativeDriver
+        duration={5000}>
+        <Video
+          source={require('../assets/video.mp4')}
           style={{flex: 1}}
-          useNativeDriver
-          duration={5000}>
-          <Video
-            source={require('../assets/video.mp4')}
-            style={{flex: 1}}
-            resizeMode="cover"
-            shouldPlay
-            muted
-            isLooping
-          />
-        </AnimatableView>
-      );
-    } else {
-      return <View style={{flex: 1}} />;
-    }
+          resizeMode="cover"
+          shouldPlay
+          muted
+          isLooping
+        />
+      </AnimatableView>
+    );
+  } else {
+    return <View style={{flex: 1}} />;
   }
 }
