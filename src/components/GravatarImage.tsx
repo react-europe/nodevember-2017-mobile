5c37adb784e538cd7d5b6151bd29dc1522b58331
m4gie <steeven.regnault@epitech.eu>
2020-04-17 10:04:47 +0200

    TypeScript: port Gravatar to TS

--- src/components/GravatarImage.tsx
+++ src/components/GravatarImage.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {StyleProp, ViewStyle} from 'react-native';
 import CachedImage from './CachedImage';
 import md5 from 'crypto-js/md5';
 
-export default function GravatarImage(props) {
+type Props = {
+  email: string;
+  style: StyleProp<ViewStyle>;
+};
+
+export default function GravatarImage(props: Props) {
   let gravatarUrl = `https://www.gravatar.com/avatar/${md5(props.email)}?s=200`;
 
   return <CachedImage source={{uri: gravatarUrl}} style={props.style} />;
