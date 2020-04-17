20ddb8832cd715563a6c2bb667c5e640cce81b1b
m4gie <steeven.regnault@epitech.eu>
2020-04-17 11:26:17 +0200

    Style: use const instead of let, sort imports

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,7 +1,8 @@
+import {Ionicons} from '@expo/vector-icons';
 import React from 'react';
 import {Platform} from 'react-native';
 import {BorderlessButton} from 'react-native-gesture-handler';
-import {Ionicons} from '@expo/vector-icons';
+
 import {Layout} from '../constants';
 
 type Props = {
