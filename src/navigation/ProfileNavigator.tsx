59f1c51402eaff0bd625cfb21ffc1cbd926515aa
m4gie <steeven.regnault@epitech.eu>
2020-04-20 10:40:11 +0200

    TypeScript: port defaultNavConfig to TS

--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 
 import Screens from '../screens';
 import DefaultStackConfig from '../utils/defaultNavConfig';
-import {ProfileStackParamList} from './types';
+import {ProfileStackParamList, ProfileProps} from './types';
 
 const Stack = createStackNavigator<ProfileStackParamList>();
 
 function ProfileNavigator() {
   return (
     <Stack.Navigator
-      screenOptions={(route) => ({...DefaultStackConfig(route)})}>
+      screenOptions={({route}: ProfileProps) => ({
+        ...DefaultStackConfig(route),
+      })}>
       <Stack.Screen name="Profile" component={Screens.Profile} />
     </Stack.Navigator>
   );
