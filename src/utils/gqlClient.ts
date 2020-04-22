3a8982ce7999af26f429d1cf8a25b38772229669
m4gie <steeven.regnault@epitech.eu>
2020-04-22 15:27:21 +0200

    TypeScript: port gqlClient to TypeScript

--- src/utils/gqlClient.ts
+++ src/utils/gqlClient.ts
@@ -1,6 +1,7 @@
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import {ApolloClient} from 'apollo-client';
 import {HttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+
 import {GQL} from '../constants';
 
 const client = new ApolloClient({
