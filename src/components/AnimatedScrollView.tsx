a1fae61e59e24661aea44cc2e2471fde91517bb4
m4gie <steeven.regnault@epitech.eu>
2020-04-23 15:05:52 +0200

    TypeScript: port AnimatedScrollView to TS

--- src/components/AnimatedScrollView.tsx
+++ src/components/AnimatedScrollView.tsx
@@ -1,8 +1,12 @@
+import {useScrollToTop} from '@react-navigation/native';
 import React from 'react';
 import {Animated, ScrollView} from 'react-native';
-import {useScrollToTop} from '@react-navigation/native';
 
-class ScrollViewRef extends React.Component {
+type ScrollViewRefProps = {
+  scrollRef: React.RefObject<ScrollView>;
+};
+
+class ScrollViewRef extends React.Component<ScrollViewRefProps> {
   render() {
     return <ScrollView ref={this.props.scrollRef} {...this.props} />;
   }
@@ -10,8 +14,8 @@ class ScrollViewRef extends React.Component {
 
 const AnimatedComponent = Animated.createAnimatedComponent(ScrollViewRef);
 
-const AnimatedScrollView = function(props) {
-  const ref = React.useRef(null);
+const AnimatedScrollView = function (props) {
+  const ref = React.useRef<ScrollView>(null);
 
   useScrollToTop(ref);
 
