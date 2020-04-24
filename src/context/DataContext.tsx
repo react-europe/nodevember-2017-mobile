2c3565c4fd76d6f88414d65698c394b3c6b4aba4
m4gie <steeven.regnault@epitech.eu>
2020-04-24 09:44:07 +0200

    TypeScript: port DataContext to TS

---
+++ src/context/DataContext.tsx
@@ -0,0 +1,27 @@
+import React, {createContext} from 'react';
+
+import {Event} from '../data/data';
+
+type DataContext = {
+  initialLinkingUri?: string;
+  event?: Event;
+};
+
+const DataContext = createContext<DataContext>({
+  initialLinkingUri: '',
+  event: {},
+});
+
+export const withData = <P extends DataContext>(
+  Component: React.ComponentType<P>
+): React.FC<Pick<P, Exclude<keyof P, keyof DataContext>>> => (
+  props: Pick<P, Exclude<keyof P, keyof DataContext>>
+) => {
+  return (
+    <DataContext.Consumer>
+      {(data) => <Component {...(props as P)} {...data} />}
+    </DataContext.Consumer>
+  );
+};
+
+export default DataContext;
