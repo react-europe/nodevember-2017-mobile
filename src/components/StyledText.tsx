859551033916579574b4c44db21c872af473f9a5
m4gie <steeven.regnault@epitech.eu>
2020-05-04 15:55:53 +0200

    UI: add accent color

--- src/components/StyledText.tsx
+++ src/components/StyledText.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleProp, TextStyle} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Text, withTheme, Theme} from 'react-native-paper';
 
 import {FontSizes} from '../constants';
 
@@ -8,35 +8,44 @@ type Props = {
   style?: StyleProp<TextStyle>;
   children: React.ReactNode;
   fontSize?: keyof typeof FontSizes;
+  accent?: boolean;
 };
 
 interface CustomTextProps extends Props {
   fontFamily: string;
+  theme: Theme;
 }
 
 function CustomText<P extends CustomTextProps>(props: P) {
+  const colors = props.theme.colors;
   const fontSize = props.fontSize ? FontSizes[props.fontSize] : FontSizes.title;
+  const textColor = props.accent ? colors.accent : colors.text;
   return (
     <Text
       {...(props as P)}
       style={[
         {backgroundColor: 'transparent'},
-        props.style,
+        {color: textColor},
         {fontSize},
         {fontFamily: props.fontFamily},
+        props.style,
       ]}
     />
   );
 }
 
+const CustomTextWithTheme = withTheme(CustomText);
+
 export function RegularText<P extends Props>(props: P) {
-  return <CustomText fontFamily="open-sans" {...(props as P)} />;
+  return <CustomTextWithTheme fontFamily="open-sans" {...(props as P)} />;
 }
 
 export function SemiBoldText<P extends Props>(props: P) {
-  return <CustomText fontFamily="open-sans-semibold" {...(props as P)} />;
+  return (
+    <CustomTextWithTheme fontFamily="open-sans-semibold" {...(props as P)} />
+  );
 }
 
 export function BoldText<P extends Props>(props: P) {
-  return <CustomText fontFamily="open-sans-bold" {...(props as P)} />;
+  return <CustomTextWithTheme fontFamily="open-sans-bold" {...(props as P)} />;
 }
