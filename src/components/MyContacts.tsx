8f547bbd993f1f0f13471496f6e046adc5b3fd93
m4gie <steeven.regnault@epitech.eu>
2020-05-05 10:55:41 +0200

    UI: contact screen use react-native-paper

--- src/components/MyContacts.tsx
+++ src/components/MyContacts.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import {
-  Alert,
-  Clipboard,
-  View,
-  StyleSheet,
-  ViewStyle,
-  StyleProp,
-  TextStyle,
-} from 'react-native';
-import {RectButton} from 'react-native-gesture-handler';
+import {Alert, Clipboard, View, ViewStyle, StyleProp} from 'react-native';
 
-import {FontSizes, Colors} from '../constants';
-import {User} from '../typings/data';
+import {Attendee} from '../typings/data';
 import {getContactTwitter} from '../utils';
+import PrimaryButton from './Buttons/PrimaryButton';
 import ContactCard from './ContactCard';
 import {SemiBoldText} from './StyledText';
 
 type Props = {
-  contacts: User[];
-  tickets: User[];
+  contacts: Attendee[];
+  tickets: Attendee[];
   style?: StyleProp<ViewStyle>;
-};
-
-type ClipBorderRadiusProps = {
-  style?: StyleProp<TextStyle>;
-  children: React.ReactNode;
 };
 
 export default function MyContacts(props: Props) {
@@ -53,20 +39,13 @@ export default function MyContacts(props: Props) {
 
   return (
     <View style={[{marginHorizontal: 10}, props.style]}>
-      <SemiBoldText style={{fontSize: FontSizes.title}}>
-        My Contacts
-      </SemiBoldText>
+      <SemiBoldText fontSize="lg">My Contacts</SemiBoldText>
       {contacts && contacts.length > 0 ? (
-        <ClipBorderRadius>
-          <RectButton
-            style={styles.bigButton}
-            onPress={_handlePressCopyEmails}
-            underlayColor="#fff">
-            <SemiBoldText style={styles.bigButtonText}>
-              Copy all emails to clipboard
-            </SemiBoldText>
-          </RectButton>
-        </ClipBorderRadius>
+        <PrimaryButton onPress={_handlePressCopyEmails}>
+          <SemiBoldText fontSize="md" accent>
+            Copy all emails to clipboard
+          </SemiBoldText>
+        </PrimaryButton>
       ) : null}
       {contacts.map((contact) => {
         if (contact?.id && contact?.email) {
@@ -83,32 +62,3 @@ export default function MyContacts(props: Props) {
     </View>
   );
 }
-
-const ClipBorderRadius = ({children, style}: ClipBorderRadiusProps) => {
-  return (
-    <View style={[{borderRadius: BORDER_RADIUS, overflow: 'hidden'}, style]}>
-      {children}
-    </View>
-  );
-};
-
-const BORDER_RADIUS = 3;
-
-const styles = StyleSheet.create({
-  bigButton: {
-    backgroundColor: Colors.blue,
-    paddingHorizontal: 15,
-    height: 50,
-    marginHorizontal: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: BORDER_RADIUS,
-    overflow: 'hidden',
-    flexDirection: 'row',
-  },
-  bigButtonText: {
-    fontSize: FontSizes.normalButton,
-    color: '#fff',
-    textAlign: 'center',
-  },
-});
