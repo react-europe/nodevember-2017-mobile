58fe1a7d58fc71debb002c588aa20e1f1ea0d7e2
m4gie <steeven.regnault@epitech.eu>
2020-05-20 15:30:03 +0200

    web: display different button on web

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,7 +1,10 @@
+import '@expo/match-media';
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {RectButton} from 'react-native-gesture-handler';
 import {Theme, useTheme} from 'react-native-paper';
+import {useMediaQuery} from 'react-responsive';
+import {View} from 'react-native-animatable';
 
 type Props = {
   onPress: () => void;
@@ -10,16 +13,47 @@ type Props = {
 
 export default function PrimaryButton(props: Props) {
   const theme: Theme = useTheme();
+
+  const isTabletOrMobileDevice = useMediaQuery({
+    maxDeviceWidth: 1224,
+    query: '(max-device-width: 1224px)',
+  });
+
+  if (isTabletOrMobileDevice) {
+    return (
+      <RectButton
+        style={[styles.PrimaryButton, {backgroundColor: 'limegreen'}]}
+        onPress={props.onPress}>
+        {props.children}
+      </RectButton>
+    );
+  }
   return (
-    <RectButton
-      style={[styles.PrimaryButton, {backgroundColor: theme.colors.primary}]}
-      onPress={props.onPress}>
-      {props.children}
-    </RectButton>
+    <View style={[styles.centerContainer, {backgroundColor: 'teal'}]}>
+      <RectButton
+        style={[styles.PrimaryButtonWeb, {backgroundColor: 'salmon'}]}
+        onPress={props.onPress}>
+        {props.children}
+      </RectButton>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  centerContainer: {
+    alignItems: 'center',
+  },
+  PrimaryButtonWeb: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 15,
+    marginTop: 10,
+    overflow: 'hidden',
+    borderRadius: 3,
+    flexDirection: 'row',
+    width: '400px',
+  },
   PrimaryButton: {
     height: 50,
     justifyContent: 'center',
