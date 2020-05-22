18538cd4a2aafb720a62a60d68cfeb2ebb8e4698
m4gie <steeven.regnault@epitech.eu>
2020-05-22 09:52:58 +0200

    web: add screenWidth hook

---
+++ src/utils/useScreenWidth.ts
@@ -0,0 +1,9 @@
+import {useMediaQuery} from 'react-responsive';
+
+import {mediaQuery} from '../constants/theme';
+
+export default function useScreenWidth() {
+  const isSmall = useMediaQuery(mediaQuery);
+
+  return isSmall;
+}
