f0455147617b5908399c74431e36c273d1de0ad7
m4gie <steeven.regnault@epitech.eu>
2020-05-25 10:43:19 +0200

    web: remove link button hover effect

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -11,17 +11,9 @@ type Props = {
 export default function LinkButton({to, action, children, ...rest}: Props) {
   const {onPress, ...props} = useLinkProps({to, action});
 
-  const [isHovered, setIsHovered] = React.useState(false);
-
   if (Platform.OS === 'web') {
     return (
-      <View
-        onClick={onPress}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        style={{transitionDuration: '150ms', opacity: isHovered ? 0.5 : 1}}
-        {...props}
-        {...rest}>
+      <View onClick={onPress} {...props} {...rest}>
         <Text>{children}</Text>
       </View>
     );
