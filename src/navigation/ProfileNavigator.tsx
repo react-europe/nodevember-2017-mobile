1ae3f9abd9de6efb63883ae17c526a1423cafd0c
m4gie <steeven.regnault@epitech.eu>
2020-05-29 12:06:20 +0200

    web: Profile, add button to open the drawer

--- src/navigation/ProfileNavigator.tsx
+++ src/navigation/ProfileNavigator.tsx
@@ -1,5 +1,8 @@
+import {Ionicons} from '@expo/vector-icons';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
+import {Platform, StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
 
 import Screens from '../screens';
 import {ProfileStackParamList} from '../typings/navigation';
@@ -13,9 +16,29 @@ function ProfileNavigator() {
       screenOptions={({route}) => ({
         ...DefaultStackConfig(route),
       })}>
-      <Stack.Screen name="Profile" component={Screens.Profile} />
+      <Stack.Screen
+        name="Profile"
+        component={Screens.Profile}
+        options={({navigation}) => ({
+          headerLeft: () => (
+            <Button onPress={() => navigation.openDrawer()}>
+              <Ionicons
+                name={Platform.OS === 'ios' ? 'ios-menu' : 'md-menu'}
+                size={Platform.OS === 'ios' ? 40 : 30}
+                style={styles.icon}
+              />
+            </Button>
+          ),
+        })}
+      />
     </Stack.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    color: '#FFF',
+  },
+});
 
 export default ProfileNavigator;
