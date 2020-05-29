406abbf32188ea80e465e774405b7358a3469e7e
m4gie <steeven.regnault@epitech.eu>
2020-05-29 14:08:01 +0200

    web: add DrawerOpenButton component

---
+++ src/components/DrawerOpenButton.tsx
@@ -0,0 +1,26 @@
+import {Ionicons} from '@expo/vector-icons';
+import React from 'react';
+import {Platform, StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
+
+import {useCurrentScreenWidth} from '../utils/useScreenWidth';
+
+export default function DrawerOpenButton(navigation) {
+  const isLargeScreen = useCurrentScreenWidth();
+  if (isLargeScreen || Platform.OS !== 'web') {
+    return null;
+  }
+  return {
+    headerLeft: () => (
+      <Button onPress={() => navigation.openDrawer()}>
+        <Ionicons name="md-menu" size={30} style={styles.icon} />
+      </Button>
+    ),
+  };
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    color: '#FFF',
+  },
+});
