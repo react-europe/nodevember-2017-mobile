1fc3f7e42e3d0164bc8489ceefc62ae1cd1a53fa
m4gie <steeven.regnault@epitech.eu>
2020-05-29 13:59:38 +0200

    web: add hook to get current screen width

--- src/utils/useScreenWidth.ts
+++ src/utils/useScreenWidth.ts
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react';
+import {Dimensions, ScaledSize} from 'react-native';
 import {useMediaQuery} from 'react-responsive';
 
 import {mediaQuery} from '../constants/theme';
@@ -6,4 +8,18 @@ export default function useScreenWidth() {
   const isSmall = useMediaQuery(mediaQuery);
 
   return !isSmall;
+}
+
+export function useCurrentScreenWidth() {
+  const [dimensions, setDimensions] = useState(Dimensions.get('window'));
+
+  useEffect(() => {
+    const onDimensionsChange = ({window}: {window: ScaledSize}) => {
+      setDimensions(window);
+    };
+    Dimensions.addEventListener('change', onDimensionsChange);
+    return () => Dimensions.removeEventListener('change', onDimensionsChange);
+  }, []);
+
+  return dimensions.width >= 1024;
 }
